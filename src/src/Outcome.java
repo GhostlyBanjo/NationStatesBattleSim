a5bfde55a14399098d185ef2e21c75657ecf70bd
GhostlyBanjo <fdavis.3.klein@gmail.com>
2015-03-06 13:42:34 -0600

    Fixed the terible bug wiht the bad troop killing simulation, can retreat now too.

--- src/src/Outcome.java
+++ src/src/Outcome.java
@@ -21,15 +21,9 @@ public class Outcome {
         this.s2 = scorel;
     }
     public String toString(){
-        switch (o){
-            case WIN:return one.getName() + " has defeated " + two.getName() + "!" + "\n Score: " + s1 +" | "+ s2 + "\n Force: " + one.getForces() + "|" + two.getForces();
-            case DRAW:
-                break;
-            case LOSE:return one.getName() + " was defeated by " + two.getName() + "!" + "\n Score: " + s1 +" | "+ s2 + "\n Force: " + one.getForces() + "|" + two.getForces();
-
-
-        }
-        return one.getName() + " has defeated " + two.getName() + "!" + "\n Score: " + s1 +" | "+ s2 + "\n Force: " + one.getForces() + "|" + two.getForces();
+        if(o == OutcomeType.WIN){ WIN:return one.getName() + " has defeated " + two.getName() + "!" + "\n Score: " + s1 +" | "+ s2 + "\n Force: " + one.getForces() + "|" + two.getForces();}
+        else if(o == OutcomeType.LOSE){return one.getName() + " was defeated by " + two.getName() + "!" + "\n Score: " + s1 +" | "+ s2 + "\n Force: " + one.getForces() + "|" + two.getForces();}
+        else return "Stalemate \n Score: " + s1 +" | "+ s2 + "\n Force: " + one.getForces() + "|" + two.getForces();
     }
 
 }
